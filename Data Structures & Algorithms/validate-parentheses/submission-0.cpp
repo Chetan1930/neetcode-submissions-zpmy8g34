f9ee64de6523a8ece82e0e976cc6b7989b094a81
Chetan Chauhan <57714871+Chetan1930@users.noreply.github.com>
2026-07-15 07:39:28 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string st) {
+        stack<char>s;
+
+        for(char &i:st){
+
+            if(i==']' || i=='}' || i==')'){
+                if(s.empty())return false;
+
+                while(!s.empty()){
+                    char temp=s.top();
+                    s.pop();
+                    if(i==']' && temp=='[')break;
+                    else if(i=='}' && temp=='{')break;
+                    else if(i==')' && temp=='(')break;
+                    else{
+                        if(s.empty()) return false;
+                    }
+                }
+            }
+
+            else s.push(i);
+            
+        }
+
+
+        return s.empty();
+
+
+    
+    }
+};
