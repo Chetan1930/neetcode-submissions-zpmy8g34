f7cd11830f457b74493cb8b9655712ee0ed69c78
Chetan Chauhan <57714871+Chetan1930@users.noreply.github.com>
2026-07-27 20:29:26 +0530

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public: 
+
+    bool bins(vector<int>&v,int target){
+        int s=0;
+        int e=v.size()-1;
+
+        while(s<=e){
+            int mid= (e-s)/2 + s; 
+
+
+            if(v[mid]==target)return true;
+            else if(v[mid]<target)s=mid+1;
+            else e=mid-1;
+        }
+
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+        int s=0,e=n-1;
+        int mid =0;
+        while(s<=e){
+            mid= (e-s)/2 + s; 
+
+            if(matrix[mid][0]==target)return true;
+            else if((matrix[mid][0])>target)e=mid-1;
+            else {
+                if((matrix[mid][m-1])>target)return bins(matrix[mid],target);
+                else s=mid+1;
+            }
+        }
+
+        return false;
+    }
+};
