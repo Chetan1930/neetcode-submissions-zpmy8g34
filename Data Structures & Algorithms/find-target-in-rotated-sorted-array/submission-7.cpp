1a454c95c74c03501199c9c832315af4158f0def
Chetan Chauhan <57714871+Chetan1930@users.noreply.github.com>
2026-07-28 20:53:40 +0530

    Add: find-target-in-rotated-sorted-array - submission-7

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-7.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s=0;
+        int e=nums.size()-1;
+
+        while(s<=e){
+            int mid=(e-s)/2 + s;
+
+            if(nums[mid]==target)return mid;
+
+            else if(nums[mid]>=nums[s] ){
+              if(target<nums[mid] && target>=nums[s]){
+                e=mid-1;
+            }
+            else s=mid+1;
+            }
+
+            else if(nums[mid]<=nums[e]){
+               if(target>nums[mid] && target<=nums[e]) s=mid+1;
+               else e=mid-1;
+            }
+            else break;
+        }
+
+
+
+
+        return -1;
+    }
+};
