e66a5730decc24dac682517e97f4bb460e92fc4a
Chetan Chauhan <57714871+Chetan1930@users.noreply.github.com>
2026-08-25 12:29:56 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+
+        for(auto &i:stones){
+            pq.push(i);
+        }
+
+        while(pq.size()>1){
+            int x=pq.top();
+            pq.pop();
+            int y=pq.top();
+            pq.pop();
+            int res = x-y;
+            if(res){
+                pq.push(res);
+            }
+        }
+        return pq.top();
+    }
+};
