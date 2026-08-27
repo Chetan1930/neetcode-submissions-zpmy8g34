423e074a64ac21ca78f1b44d8e9a72ab5acc31f4
Chetan Chauhan <57714871+Chetan1930@users.noreply.github.com>
2026-08-27 16:39:25 +0530

    Add: task-scheduling - submission-4

---
+++ Data Structures & Algorithms/task-scheduling/submission-4.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int>v(26,0);
+        priority_queue<int>pq;
+        
+        for(auto &i:tasks){
+            v[i-'A']++;
+        }
+
+        for(auto &i:v){
+            if(i>0)pq.push(i);
+        }
+
+        int time=0;
+        while(!pq.empty()){
+            vector<int>temp;
+
+            for(int i=0;i<n+2;i++){
+
+                if(!pq.empty()){
+                    int freq=pq.top();
+                    pq.pop();
+                    freq--;
+
+                    temp.push_back(freq);
+                }
+            }
+
+            for(int i=0;i<temp.size();i++){
+                if(temp[i]>0)pq.push(temp[i]);
+            }
+
+            if(!pq.empty()){
+                time+= (n+1);
+            }
+            else {
+                time+=temp.size();
+            }
+        }
+
+
+        return time;
+
+        
+
+    }
+};
