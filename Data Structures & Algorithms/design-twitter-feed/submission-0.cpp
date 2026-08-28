58d0ac22c8d2f4f826b881984878c1d342457400
Chetan Chauhan <57714871+Chetan1930@users.noreply.github.com>
2026-08-28 08:03:15 +0530

    Add: design-twitter-feed - submission-0

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-0.cpp
@@ -0,0 +1,52 @@
+class Twitter {
+public:
+    unordered_map<int,vector<int>>tweet;
+    unordered_map<int,vector<int>>following;
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        tweet[userId].push_back(tweetId);
+        return ;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        priority_queue<int>pq;
+
+        for(auto &i:tweet[userId]){
+            pq.push(i);
+            if(pq.size()>10)pq.pop();
+        }
+
+        for(auto &j:following[userId])
+        for(auto &i:tweet[j]){
+            pq.push(i);
+            if(pq.size()>10)pq.pop();
+        }
+
+        vector<int>lastesttweet;
+
+        while(!pq.empty()){
+            lastesttweet.push_back(pq.top());
+            pq.pop();
+        }
+
+
+        return lastesttweet;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        following[followerId].push_back(followeeId);
+        return ;
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        vector<int>followers = following[followerId];
+        
+        erase(followers,followeeId);
+        following[followerId]=followers;
+
+        return ;
+    }
+};
