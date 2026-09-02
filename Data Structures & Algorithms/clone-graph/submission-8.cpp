01373808f5019a6965b3bceb23607f7d6a5173a6
Chetan Chauhan <57714871+Chetan1930@users.noreply.github.com>
2026-09-02 23:06:57 +0530

    Add: clone-graph - submission-8

---
+++ Data Structures & Algorithms/clone-graph/submission-8.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(node==nullptr)return nullptr;
+
+        queue<Node*>q;
+        unordered_map<Node*,Node*>mp;
+        q.push(node);
+        mp[node]=new Node(node->val);
+
+        while(!q.empty()){
+            Node* temp = q.front();
+            q.pop();
+
+            for(auto &i:temp->neighbors){
+                if(!mp.count(i)){
+                    mp[i]=new Node(i->val);
+                    q.push(i);
+                }
+                mp[temp]->neighbors.push_back(mp[i]);
+            }
+        }
+
+        
+        
+
+        return mp[node];
+    }
+};
